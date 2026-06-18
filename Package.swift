fb1df7f45ea1fb371ea31726326d293e1f7bb9e3
rahulw-os <139221004+rahulw-os@users.noreply.github.com>
2026-06-18 17:16:57 +0530

    Update osmos SDK to version 2.6.1

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "osmos",
-            url: "https://github.com/onlinesales-ai/osmos-ios-sdk/releases/download/osmos-sdk-v2.6.0/osmos-ios-sdk-2.6.0.zip",
-            checksum: "5337f098386183b0d3b71c6f70c6e9e6a2575bd9bfeab4a05757a4f40fcb168a"
+            url: "https://github.com/onlinesales-ai/osmos-ios-sdk/releases/download/osmos-sdk-v2.6.1/osmos-ios-sdk-2.6.1.zip",
+            checksum: "183a8f208347e9ce581b4a4c16601d0770bb9af003800d2bec596649c21010c5"
         )
 
     ]
